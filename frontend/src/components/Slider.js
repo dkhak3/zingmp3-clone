9e7fca2e83c14d274b9da910ec47b6996562bc0b
Nguyễn Hữu Duy Kha <94631848+dkhak3@users.noreply.github.com>
2024-01-04 20:45:04 +0700

    Coding slick slider using javascript - Adding animation

--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -4,46 +4,88 @@ import { getArrSlider } from "../ultis/fn";
 
 const Slider = () => {
   const { banner } = useSelector((state) => state.app);
-  console.log("🚀 ~ file: Slider.js:6 ~ Slider ~ banner:", banner);
 
+  // ainimation for banner
   useEffect(() => {
     const sliderEls = document.getElementsByClassName("slider-item");
     let min = 0;
     let max = 2;
+
     const intervalId = setInterval(() => {
       const list = getArrSlider(min, max, sliderEls.length - 1);
+
       for (let i = 0; i < sliderEls.length; i++) {
-        list.some((item) => item === i)
-          ? (sliderEls[i].style.display = "block")
-          : (sliderEls[i].style.display = "none");
-      }
-      if (min === sliderEls.length - 1) {
-        min = 0;
-      } else {
-        min += 1;
+        // Delete classnames (css)
+        sliderEls[i]?.classList?.remove(
+          "animate-slide-right",
+          "order-last",
+          "z-20"
+        );
+        sliderEls[i]?.classList?.remove(
+          "animate-slide-left",
+          "order-first",
+          "z-10"
+        );
+        sliderEls[i]?.classList?.remove(
+          "animate-slide-left2",
+          "order-2",
+          "z-10"
+        );
+
+        // Hide or Show images
+        if (list.some((item) => item === i)) {
+          sliderEls[i].style.cssText = `display: block`;
+        } else {
+          sliderEls[i].style.cssText = `display: none`;
+        }
       }
 
-      if (max === sliderEls.length - 1) {
-        max = 0;
-      } else {
-        max += 1;
-      }
-    }, 1000);
+      // Add animation by adding classnames
+      list.forEach((item) => {
+        if (item === max) {
+          sliderEls[item]?.classList?.add(
+            "animate-slide-right",
+            "order-last",
+            "z-20"
+          );
+        } else if (item === min) {
+          sliderEls[item]?.classList?.add(
+            "animate-slide-left",
+            "order-first",
+            "z-10"
+          );
+        } else {
+          sliderEls[item]?.classList?.add(
+            "animate-slide-left2",
+            "order-2",
+            "z-10"
+          );
+        }
+      });
+
+      min = min === sliderEls.length - 1 ? 0 : min + 1;
+      max = max === sliderEls.length - 1 ? 0 : max + 1;
+    }, 3000);
+
     return () => {
       intervalId && clearInterval(intervalId);
     };
   }, []);
 
   return (
-    <div className="flex gap-4 w-full overflow-hidden px-[59px] pt-8">
-      {banner?.map((item) => (
-        <img
-          key={item.encodeId}
-          src={item.banner}
-          alt=""
-          className="slider-item flex-1 object-contain w-1/3 rounded-lg"
-        />
-      ))}
+    <div className="w-full overflow-hidden px-[59px]">
+      <div className="flex gap-8 w-full pt-8">
+        {banner?.map((item, index) => (
+          <img
+            key={item.encodeId}
+            src={item.banner}
+            alt=""
+            className={`slider-item flex-1 object-contain w-[30%] rounded-lg ${
+              index <= 2 ? "block" : "hidden"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
